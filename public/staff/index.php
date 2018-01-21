f519964c79597f99e7d95af7146dcd4a670b0e62
dan-stachlewski <dan.stachlewski@gmail.com>
2018-01-21 21:19:19 +1100

    Added the basic markup for the staff index.php landing page.

--- public/staff/index.php
+++ public/staff/index.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>GBI</title>
+    <link rel="stylesheet" media="" href="../stylesheets/staff.css">
 </head>
 <body>
+    <header>
+        <h1>GBI Staff Area</h1>
+    </header>
+    <navigation>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+            <!-- <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li> -->
+        </ul>
+    </navigation>
+    <footer>
+        &copy; <?= date('Y') ?> Globe Bank.
+    </footer>
 
 </body>
 </html>
