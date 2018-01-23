57029c6325db736621a9f9f197360d152708e74d
dan-stachlewski <dan.stachlewski@gmail.com>
2018-01-23 12:00:56 +1100

    Re-worked the show.php page, providing  with default values if not set to avoid breaking the page if for some reason URL Params are not passed via the URL. We used the Ternary Operator to check if  were sent or not & provide default valuse if not

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -2,9 +2,28 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
 <?php
+// If for some reason someone types in /globe_bank/public/staff/subjects/show.php without any URL Params after .show.php, to avoid throwing errors we need to check if the $variables have been defined. If they have (and there are parameters in the URL the page works as normal. If however no parameters have been set as per above scenario, then we need to define default values.
+//The below code shows 3 ways of doing just that!
 
-$id = $_GET['id'];
-$title = $_GET['menu_name'];
+// PHP 5.4 back
+/*
+if(isset($_GET['id']) && isset($_GET['menu_name'])){
+    $id = $_GET['id'];
+    $title = $_GET['menu_name'];
+}else{
+    $id = '1';
+    $title = 'About Globe Bank';
+}
+*/
+
+// PHP 5.5 onwards
+$id = isset($_GET['id']) ? $_GET['id'] : '1';
+$title = isset($_GET['menu_name']) ? $_GET['menu_name'] : 'About Globe Bank';
+
+// PHP 7.0 onwards - using NULL COALESCING OPERATOR
+// $id = $_GET['id'] ?? '1';
+// $title = $_GET['menu_name'] ?? 'About Globe Bank';
+
 echo "<h1>Page ID: $id</h1>";
 echo "<h2>Page Title: $title</h2>";
 ?>
