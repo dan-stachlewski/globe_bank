2e061edada301292941d3969c2a92b16225d5184
dan-stachlewski <dan.stachlewski@gmail.com>
2018-01-23 11:32:30 +1100

    Set up the View link and using the 'id' url parameter. The 'id' is passed via the URL. This ensures the user views the correct information about the subject of inquiry.

--- public/staff/subjects/index.php
+++ public/staff/subjects/index.php
@@ -21,7 +21,42 @@ $subjects = [
 <?php include(SHARED_PATH . '/staff_header.inc.php'); ?>
 <!-- This allows us to define what is unique to this page with the section below -->
 <section id="content">
-    
+    <div class="subjects listing">
+        <h1>Subjects</h1>
+        <div class="actinos">
+            <a href="" class="actions">Create New Subject</a>
+        </div>
+        <!-- This table has a list of subjects that will be displayed in the sidebar with data being retrieved from the database. For now the menu has been set up with an associative array which emulates the database data -->
+        <table class="list">
+            <tr>
+                <th>ID</th>
+                <th>Position</th>
+                <th>Visible</th>
+                <th>Name</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
+            </tr>
+            <!-- We are using a foreach loop to iterate through the array and display the data based on key=>value pairs. For each value the array will loop through, find the key & insert the associated value for that key generating the menu -->
+
+            <!-- For each of the $subjects in the associative-array we are creating an array called $subject where we can ask for the values for each of the keys to be displayed in the table row. Thus, while there are items in the array, the for each loop will iterate through and display them in their own row. -->
+            <?php foreach($subjects as $subject) { ?>
+            <tr>
+                <td><?=$subject['id']; ?></td>
+                <td><?=$subject['position']; ?></td>
+                <td><?=$subject['visible'] == 1 ? 'true':'false'; ?></td>
+                <td><?=$subject['menu_name']; ?></td>
+                <!-- For each view the link needs to have it's own id associated with that subject. We can use a url parameter - the id via the url so that we can view the details for each subject.  -->
+                <!-- For multiple parameters we need to assign the name of each param with an & b/w them ie: show.php?id='<$variable>'&'menu_name'$<variable>' -->
+                <td><a href="<?= url_for('/staff/subjects/show.php?id=' . $subject['id'] . '&' . 'menu_name=' . $subject['menu_name']); ?> " class="action">View</a></td>
+                <td><a href="" class="action">Edit</a></td>
+                <td><a href="" class="action">Delete</a></td>
+            </tr>
+        <!-- Don't forget to close off the loop at the end of the row... -->
+            <?php } ?>
+        </table>
+    </div><!-- /END .subjects -->
+
 
 </section>
 <!-- We are including the footer here as it is not dynamic - it is 'static'/re-usable -->
